86daae9a0243078d3785f798ad0f4f4641fd9bc0
Paco <pacocastro608@gmail.com>
2019-05-07 13:54:07 -0500

    pre log in

--- view/home/js.js
+++ view/home/js.js
@@ -8,4 +8,19 @@ window.addEventListener("load", function(event) {
 		x.text = 'Log Out';
 	}
   });
+
+$("#login").click(function(){
+	if(localStorage.getItem('token') == null){
+		//se lleva a la pagina de sign in
+		alert("sesion iniciada");
+		//hay que borrar esta madre
+		localStorage.setItem('token',"asd");
+	}
+	else {
+		//se lleva al home y se borra el token hay que hacer sign in 
+		//este ya esta listo creo
+		localStorage.removeItem('token');
+		location.reload();
+	}
 	
+});
