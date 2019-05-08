1958004ab209130ae901a98d53c19082d19ec476
Juan <a00815058@itesm.mx>
2019-05-08 12:00:35 -0500

    update movie details

---
+++ view/movieDetails/js.js
@@ -0,0 +1,76 @@
+var url_string = window.location.href
+var url = new URL(url_string);
+var c = url.searchParams.get("id");
+$(function(){
+	$.ajax({
+		url : 'http://localhost:3000/movies/' + c,
+		type : "GET", 
+		dataType : "json",
+		success: function(data){
+			var new_html = ` <div class="col" id="img-col">
+        <img src="${data.poster}" class="img-fluid">
+      </div>
+      <div class="col">
+        <h1>${data.title}</h1>
+        <br>
+        <h2>${data.director}</h2>
+        <br>
+        <ul>`;
+        for(var i = 0; i < data.powers.length; i++){
+        	new_html+=`<li> ${data.powers[i]}</li>`;
+        }
+ 		new_html+=`
+        </ul>
+        <br>`;
+        if(data.status == 'Alive'){
+        	new_html+=`<p id="alive">${data.status}<p>`;
+        } else {
+        	new_html+=`<p id="dead">${data.status}<p>`;
+        }
+        
+        new_html+=`
+        <br>
+        <h3>Who is ${data.aliases[0]}?</h3>
+         <p>${data.biography}</p>
+        <br>
+        <p>${data.age} years</p>
+        <br>
+        <p>Creators: ${data.creators}</p>
+        <br>
+        <h3>Actor</h3>
+        <p>${data.actor}</p>
+      	</div>`;
+      	$("#charDet").append(new_html);
+      	},
+    	error: function(data){
+    		<h1>error</h1>
+    	}
+	});
+});
+
+window.addEventListener("load", function(event) {
+    if(localStorage.getItem('token') == null){
+        x = document.getElementById("login");
+        x.text = "Log In";
+    }
+    else {
+        x = document.getElementById("login");
+        x.text = 'Log Out';
+    }
+  });
+
+$("#login").click(function(){
+    if(localStorage.getItem('token') == null){
+        //se lleva a la pagina de sign in
+        alert("sesion iniciada");
+        //hay que borrar esta madre
+        localStorage.setItem('token',"asd");
+    }
+    else {
+        //se lleva al home y se borra el token hay que hacer sign in 
+        //este ya esta listo creo
+        localStorage.removeItem('token');
+        window.location= ("../home/index.html");
+    }
+    
+});
--- view/movies/js.js
+++ view/movies/js.js
@@ -7,8 +7,8 @@ $(function(){
 			let new_html = ""
 			for(let i = 0; i < data.length; i++){
 				new_html += `
-				<div class="col-lg-3 col-md-4 col-xs-6 thumb zoom">
-				<a href="../movieDetails/movieDetails.html">
+				<div class="col-lg-3 col-md-4 col-xs-6 thumb zoom" id="${data[i]._id}">
+				<a /*href="../movieDetails/movieDetails.html*/">
                     <img  src="${data[i].poster}" class="img-fluid "  alt="">
                 </a>
                 <h3 class="shname">${data[i].title}</h3> 
@@ -20,5 +20,39 @@ $(function(){
     	error: function(data){
     		//do something
     	}
+	}).done(function(response){
+		$('.zoom').click(function(event){    
+    	var id = event.currentTarget.id;
+    	localStorage.setItem("movie", id);
+    	window.location = ("../movieDetails/movieDetails.html?id="+id);
+		});
 	});
+});
+
+window.addEventListener("load", function(event) {
+    if(localStorage.getItem('token') == null){
+		x = document.getElementById("login");
+		x.text = "Log In";
+	}
+	else {
+		x = document.getElementById("login");
+		x.text = 'Log Out';
+	}
+  });
+	
+$("#login").click(function(){
+	if(localStorage.getItem('token') == null){
+		//se lleva a la pagina de sign in
+		alert("sesion iniciada");
+		//hay que borrar esta madre
+		localStorage.setItem('token',"asd");
+	}
+	else {
+		//se lleva al home y se borra el token hay que hacer sign in 
+		//este ya esta listo creo
+		localStorage.removeItem('token');
+		window.location= ("../home/index.html");
+		location.reload();
+	}
+	
 });
