1611f230faf8b08e7ae150beecceff0d7fcff086
Paco <pacocastro608@gmail.com>
2019-05-10 15:39:30 -0500

    log in

--- view/logIn/login.js
+++ view/logIn/login.js
@@ -1,9 +1,8 @@
-
-
 $('#btn').click(function(){
 	var email = document.getElementById("inputEmail");
 	var pass = document.getElementById("inputPassword");
-	$.ajax({
+	if ( validateEmail(email.value) && pass.value != null){
+		$.ajax({
 			type : 'POST',
 			url : 'https://themcuproject.herokuapp.com/users/login',
 			crossDomain: true,
@@ -17,7 +16,15 @@ $('#btn').click(function(){
 				window.location = ("../home/index.html");
 			},
 			error:function(error){
-				alert("NO SE PUDO INICIAR SESION");		
+				alert("Email y/o contraseña incorrectos. Verificalos pls");		
 			}
 		});
+	} else {
+		alert("Email y/o contraseña invalidos")
+	}
 });
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
