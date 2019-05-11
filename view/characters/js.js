2731b3ce0b82c06515c2c742e861db7c31814c2f
Oscar Guevara <guevara1697@gmail.com>
2019-05-11 17:06:54 -0500

    Add Movie Button

--- view/characters/js.js
+++ view/characters/js.js
@@ -1,6 +1,6 @@
 $.ajax({
 		url : 'https://themcuproject.herokuapp.com/characters',
-		type : "GET", 
+		type : "GET",
 		dataType : "json",
 		success: function(data){
 			if(localStorage.getItem('admin') != null){
@@ -11,14 +11,14 @@ $.ajax({
    			}
 
 			let new_html = ""
-			new_html +=  `<div class="row" id="images">`;	
-			for(let i = 0; i <data.length; i++){		
-    			new_html += `
+			new_html +=  `<div class="row" id="images">`;
+			for(let i = 0; i < data.length; i++){
+    		new_html += `
 				<div class="col-lg-3 col-md-4 col-xs-6 thumb zoom" id="${data[i]._id}">
 				<a href="#">
                     <img  src="${data[i].profilePhoto}" class="img-fluid " alt="">
                 </a>
-                <h3 class="shname">${data[i].aliases[1]}</h3> 
+                <h3 class="shname">${data[i].aliases[1]}</h3>
             	</div>
 				`;
 			}
@@ -29,7 +29,7 @@ $.ajax({
     		//do something
     	}
 	}).done(function(response){
-		$('.zoom').click(function(event){ 
+		$('.zoom').click(function(event){
     	var id = event.currentTarget.id;
     	window.location = ("../characterDetails/characterDetails.html?id="+id);
 		});
@@ -47,18 +47,18 @@ window.addEventListener("load", function(event) {
 	}
 
   });
-	
+
 $("#login").click(function(){
 	if(localStorage.getItem('token') == null){
 		//se lleva a la pagina de sign in
 		window.location = ("../login/login.html");
 	}
 	else {
-		//se lleva al home y se borra el token hay que hacer sign in 
+		//se lleva al home y se borra el token hay que hacer sign in
 		//este ya esta listo creo
 		localStorage.removeItem('token');
 		localStorage.removeItem('admin');
 		window.location= ("../home/index.html");
 	}
-	
+
 });
--- view/movies/js.js
+++ view/movies/js.js
@@ -1,27 +1,34 @@
 $(function(){
 	$.ajax({
 		url : 'https://themcuproject.herokuapp.com/movies',
-		type : "GET", 
+		type : "GET",
 		dataType : "json",
 		success: function(data){
+			if(localStorage.getItem('admin') != null){
+				var newh = "";
+        		newh += `<button type="button" id="btn" class="btn btn-primary">Add Movie</button>`;
+        		newh += `<div><p><br> </p> </div>`;
+       		 	$("#cont").append(newh);
+   			}
 			let new_html = ""
+			new_html +=  `<div class="row" id="images">`;	
 			for(let i = 0; i < data.length; i++){
 				new_html += `
 				<div class="col-lg-3 col-md-4 col-xs-6 thumb zoom" id="${data[i]._id}">
 				<a /*href="../movieDetails/movieDetails.html*/">
                     <img  src="${data[i].poster}" class="img-fluid "  alt="">
                 </a>
-                <h3 class="shname">${data[i].title}</h3> 
+                <h3 class="shname">${data[i].title}</h3>
             </div>
 				`;
 			}
-			$("#images").append(new_html);
+			$("#cont").append(new_html);
     	},
     	error: function(data){
     		//do something
     	}
 	}).done(function(response){
-		$('.zoom').click(function(event){    
+		$('.zoom').click(function(event){
     	var id = event.currentTarget.id;
     	localStorage.setItem("movie", id);
     	window.location = ("../movieDetails/movieDetails.html?id="+id);
@@ -39,18 +46,18 @@ window.addEventListener("load", function(event) {
 		x.text = 'Log Out';
 	}
   });
-	
+
 $("#login").click(function(){
 	if(localStorage.getItem('token') == null){
 		window.location = ("../login/login.html");
 	}
 	else {
-		//se lleva al home y se borra el token hay que hacer sign in 
+		//se lleva al home y se borra el token hay que hacer sign in
 		//este ya esta listo creo
 		localStorage.removeItem('token');
 		localStorage.removeItem('admin');
 		window.location= ("../home/index.html");
 		location.reload();
 	}
-	
+
 });
