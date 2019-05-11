fb3a5036bba0b71191ad1beb8ba75dd6965d171c
Paco <pacocastro608@gmail.com>
2019-05-11 14:13:19 -0500

    edit char

--- view/characters/js.js
+++ view/characters/js.js
@@ -3,9 +3,17 @@ $.ajax({
 		type : "GET", 
 		dataType : "json",
 		success: function(data){
+			if(localStorage.getItem('admin') != null){
+				var newh = "";
+        		newh += `<button type="button" id="btn" class="btn btn-primary">Add Character</button>`;
+        		newh += `<div><p><br> </p> </div>`;
+       		 	$("#cont").append(newh);
+   			}
+
 			let new_html = ""
-			for(let i = 0; i <data.length; i++){
-				new_html += `
+			new_html +=  `<div class="row" id="images">`;	
+			for(let i = 0; i <data.length; i++){		
+    			new_html += `
 				<div class="col-lg-3 col-md-4 col-xs-6 thumb zoom" id="${data[i]._id}">
 				<a href="#">
                     <img  src="${data[i].profilePhoto}" class="img-fluid " alt="">
@@ -14,7 +22,8 @@ $.ajax({
             	</div>
 				`;
 			}
-			$("#images").append(new_html);
+			new_html+= `</div>`;
+			$("#cont").append(new_html);
     	},
     	error: function(data){
     		//do something
@@ -36,6 +45,7 @@ window.addEventListener("load", function(event) {
 		x = document.getElementById("login");
 		x.text = 'Log Out';
 	}
+
   });
 	
 $("#login").click(function(){
