61eb7bcdc22551f2ec5746a2d60fed7f4960be1f
Oscar Guevara <guevara1697@gmail.com>
2019-05-11 23:46:04 -0500

    Botones Delete

--- view/characters/js.js
+++ view/characters/js.js
@@ -3,27 +3,43 @@ $.ajax({
 		type : "GET",
 		dataType : "json",
 		success: function(data){
-			if(localStorage.getItem('admin') != null){
+			if(localStorage.getItem('admin') != null) {
 				var newh = "";
         		newh += `<button type="button" id="btn" class="btn btn-primary">Add Character</button>`;
         		newh += `<div><p><br> </p> </div>`;
        		 	$("#cont").append(newh);
-   			}
 
-			let new_html = ""
-			new_html +=  `<div class="row" id="images">`;
-			for(let i = 0; i < data.length; i++){
-    		new_html += `
-				<div class="col-lg-3 col-md-4 col-xs-6 thumb zoom" id="${data[i]._id}">
-				<a href="#">
-                    <img  src="${data[i].profilePhoto}" class="img-fluid " alt="">
-                </a>
-                <h3 class="shname">${data[i].aliases[1]}</h3>
-            	</div>
-				`;
-			}
-			new_html+= `</div>`;
-			$("#cont").append(new_html);
+						let new_html = ""
+						new_html +=  `<div class="row container" id="images">`;
+						for(let i = 0; i < data.length; i++){
+							new_html += `
+							<div class="col-lg-3 col-md-4 col-xs-6 thumb zoom" id="${data[i]._id}">
+								<a href="#">
+									<img  src="${data[i].profilePhoto}" class="img-fluid " alt="">
+			        	</a>
+								<button class="deleteBtn">Delete</button>
+			        	<h3 class="shname">${data[i].aliases[1]}</h3>
+			        </div>
+							`;
+						}
+						new_html += `</div>`;
+						$("#cont").append(new_html);
+   			} else {
+					let new_html = ""
+					new_html +=  `<div class="row" id="images">`;
+					for(let i = 0; i < data.length; i++){
+		    		new_html += `
+						<div class="col-lg-3 col-md-4 col-xs-6 thumb zoom" id="${data[i]._id}">
+							<a href="#">
+		            <img  src="${data[i].profilePhoto}" class="img-fluid " alt="">
+		          </a>
+		          <h3 class="shname">${data[i].aliases[1]}</h3>
+		        </div>
+						`;
+					}
+					new_html+= `</div>`;
+					$("#cont").append(new_html);
+				}
     	},
     	error: function(data){
     		//do something
